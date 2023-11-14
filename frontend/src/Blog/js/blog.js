cb21aa250ff73f818cd8e95170db59160fdf9bae
Soe Moe Htet <mobilesoemoehtet11@gmail.com>
2023-11-14 07:32:55 +0700

    change file names to be more readable

---
+++ frontend/src/Blog/js/blog.js
@@ -0,0 +1,80 @@
+/*----------------------------AutoTyping Effect----------------------------*/ 
+const dynamicText = document.querySelector("div span");
+const words = ["<Code>", "<Build>", "<Future>", "<Test>", "<Deploy>", "<Debug>"];
+
+// Variables to track the position and deletion status of the word
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+    const currentWord = words[wordIndex];
+    const currentChar = currentWord.substring(0, charIndex);
+    dynamicText.textContent = currentChar;
+    dynamicText.classList.add("stop-blinking");
+
+    if (!isDeleting && charIndex < currentWord.length) {
+        // If condition is true, type the next character
+        charIndex++;
+        setTimeout(typeEffect, 50);
+    } else if (isDeleting && charIndex > 0) {
+        // If condition is true, remove the previous character
+        charIndex--;
+        setTimeout(typeEffect, 50);
+    } else {
+        // If word is deleted then switch to the next word
+        isDeleting = !isDeleting;
+        dynamicText.classList.remove("stop-blinking");
+        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+        setTimeout(typeEffect, 1200);
+    }
+}
+
+typeEffect();
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------------Lecturers----------------------------*/ 
+// Swipper js to get the scrolling effect
+var swiper = new Swiper(".slide-container", {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      sliderPerGroup: 4,
+      loop: true,
+      centerSlide: "true",
+      fade: "true",
+      grabCursor: "true",
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        520: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1000: {
+          slidesPerView: 4,
+        },
+      },
+    });
+    
