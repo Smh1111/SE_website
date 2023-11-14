9d17e8379a8637141f92e392519c27c3c2fc5410
Soe Moe Htet <mobilesoemoehtet11@gmail.com>
2023-11-15 00:18:25 +0700

    Blog feature finished

--- frontend/src/Blog/js/blog.js
+++ frontend/src/Blog/js/blog.js
@@ -1,120 +1,66 @@
-/*----------------------------AutoTyping Effect----------------------------*/ 
-const dynamicText = document.querySelector("div span");
-const words = ["<Code>", "<Build>", "<Future>", "<Test>", "<Deploy>", "<Debug>"];
 
-// Variables to track the position and deletion status of the word
-let wordIndex = 0;
-let charIndex = 0;
-let isDeleting = false;
+/*----------------------------fetching Data and  populating Data----------------------------*/
 
-const typeEffect = () => {
-    const currentWord = words[wordIndex];
-    const currentChar = currentWord.substring(0, charIndex);
-    dynamicText.textContent = currentChar;
-    dynamicText.classList.add("stop-blinking");
+document.addEventListener("DOMContentLoaded", function () {
+  const apiUrl = "http://127.0.0.1:8000/blogs/all";
+  getAllBlogs();
 
-    if (!isDeleting && charIndex < currentWord.length) {
-        // If condition is true, type the next character
-        charIndex++;
-        setTimeout(typeEffect, 50);
-    } else if (isDeleting && charIndex > 0) {
-        // If condition is true, remove the previous character
-        charIndex--;
-        setTimeout(typeEffect, 50);
-    } else {
-        // If word is deleted then switch to the next word
-        isDeleting = !isDeleting;
-        dynamicText.classList.remove("stop-blinking");
-        wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-        setTimeout(typeEffect, 1200);
-    }
-}
+  function getAllBlogs() {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((blogList) => {
+        const sortedBlogList = blogList.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
 
-typeEffect();
+        // Get the article container element
+        const blogContainer = document.getElementById("blogContainer");
 
-/*----------------------------fetching Data and  populating Data----------------------------*/ 
-const apiUrl = "http://127.0.0.1:8000/blogs/all"; 
+        // Display only the four most recent blog articles
+        const recentBlogs = sortedBlogList.slice(0, 4);
 
-/*
-fetch('https://example.com/', {
-  method: 'GET',
-  body: new FormData(),
-});
-*/
-function getAllBlogs() {
-	/*
-	class Blog(BaseModel):
-		ID: int
-		title: str
-		Blocks: object
-		publishedAt: str
-
-	*/
-	/*
-	const newBlog = {
-		title: blogTitle,
-		Blocks: blogBlocks,
-		publishedAt: blogPublishedAt,
-	};
-      */ 
-	fetch(apiUrl)
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`HTTP error! Status: ${response.status}`);
-			}
-			return response.json();
-		})
-		.then((data) => {
-			// Assuming data is an array of objects with properties blogTitle and blogDate
-			data.forEach((item) => {
-				console.log("ID :" + item.id + "\ntitle : " + item.title + "\nblocks : " + item.blocks + "\npublished At : " + item.publishedAt);
-				populateData(item.id, item.title, item.publishedAt);
-			});
-
-			// Process the data received from the API
-		})
-		.catch((error) => {
-			console.error("Fetch error:", error);
-		});
-}
-document.addEventListener("DOMContentLoaded", function() {
-  // Article data (you can replace this with your actual data)
-  const articles = [
-    {
-      id: 0,
-      title: "Our first office",
-      imageUrl: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-1.png",
-      description: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
-      readTime: "2 minutes",
-      link: "#"
-    },
-    // Add more articles as needed
-  ];
+        // Iterate over the articles and append them to the container
+        recentBlogs.forEach((blog) => {
+            const blogHTML = createArticleHTML(blog);
+            blogContainer.innerHTML += blogHTML;
+        });
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }
 
   // Function to generate HTML for an article
-  function createArticleHTML(article) {
+  function createArticleHTML(blog) {
+    const imageURL = "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-1.png";
+    const eachBlogLink = "../Blog/eachBlog.html";
+
+    // Create a unique identifier for each blog link
+    const linkId = `blogLink_${blog.id}`;
+
+    // Return a template string with HTML structure
     return `
       <article class="max-w-xs">
-        <a href="${article.link}">
-          <img src="${article.imageUrl}" class="mb-5 rounded-lg" alt="Image">
+        <a href="${eachBlogLink}" id="${linkId}" onclick="storeBlogId(${blog.id})">
+          <img src="${imageURL}" class="mb-5 rounded-lg" alt="Image">
         </a>
         <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-          <a href="${article.link}">${article.title}</a>
+          <a href="${eachBlogLink}" onclick="storeBlogId(${blog.id})">${blog.title}</a>
         </h2>
-        <p class="mb-4 text-gray-500 dark:text-gray-400">${article.description}</p>
-        <a href="${article.link}" class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
-          Read in ${article.readTime}
+        <p class="mb-4 text-gray-500 dark:text-gray-400">${blog.publishedAt}</p>
+        <a href="${eachBlogLink}" onclick="storeBlogId(${blog.id})" class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline">
+          Read
         </a>
       </article>
     `;
   }
 
-  // Get the article container element
-  const articleContainer = document.getElementById("articleContainer");
-
-  // Iterate over the articles and append them to the container
-  articles.forEach(article => {
-    const articleHTML = createArticleHTML(article);
-    articleContainer.innerHTML += articleHTML;
-  });
+  
 });
+// Function to store the blog ID in session storage
+function storeBlogId(blogId) {
+  sessionStorage.setItem('blogId', blogId);
+}
--- frontend/src/Blog/js/eachBlog.js
+++ frontend/src/Blog/js/eachBlog.js
@@ -1,28 +1,14 @@
-/*---------------------------Using the GET api----------------------------- */
-const apiUrl = "http://127.0.0.1:8000/blogs/2";
+// sessionStorage.setItem("BlogId", 1);
 
-/*
-fetch('https://example.com/', {
-  method: 'GET',
-  body: new FormData(),
+document.addEventListener("DOMContentLoaded", async () => {
+	// Retrieve the blogId from sessionStorage
+	blogId = sessionStorage.getItem("blogId");
+
+	getBlogById_Request(blogId);
 });
-*/
-function getBlogByID() {
-	/*
-	class Blog(BaseModel):
-		ID: int
-		title: str
-		Blocks: object
-		publishedAt: str
 
-	*/
-	/*
-	const newBlog = {
-		title: blogTitle,
-		Blocks: blogBlocks,
-		publishedAt: blogPublishedAt,
-	};
-      */
+function getBlogById_Request(id) {
+	const apiUrl = "http://127.0.0.1:8000/blogs/" + id;
 	fetch(apiUrl)
 		.then((response) => {
 			if (!response.ok) {
@@ -30,17 +16,34 @@ function getBlogByID() {
 			}
 			return response.json();
 		})
-		.then((data) => {
-			console.log(data);
-
-			const edjsParser = edjsHTML();
-			const HTML = edjsParser.parse(data.blocks);
-			// returns array of html strings per block
-			console.log(HTML);
+		.then((blog) => {
+			populateArticle(blog);
 		})
 		.catch((error) => {
 			console.error("Fetch error:", error);
 		});
-}
 
-getBlogByID();
+	// Function to populate the article with blog data
+	function populateArticle(blog) {
+		// blog.id, blog.title, blog.blocks, blog.publishedAt
+		const articleContainer = document.getElementById("eachBlog");
+
+		const edjsParser = edjsHTML();
+		const blog_HTML_contents_Array = edjsParser.parse(blog.blocks);
+
+		var combinedString = blog_HTML_contents_Array.join(" ");
+
+		articleContainer.innerHTML = `
+      <header class="mb-4 lg:mb-6 not-format">
+        <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+          ${blog.title}
+        </h1>
+	  <p class="text-gray-600 dark:text-gray-400 text-sm">${blog.publishedAt}</p>
+
+      </header>
+      
+     	${combinedString}
+    `;
+
+	}
+}
